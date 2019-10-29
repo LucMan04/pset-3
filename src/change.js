7f44d4784dd56be8ce684f913bd5260331d6f607
Lucas Osiecki <LOsiecki@ucvts.tec.nj.us>
2019-10-29 13:20:50 -0400

    first

--- src/change.js
+++ src/change.js
@@ -2,13 +2,35 @@ const readlineSync = require("readline-sync");
 
 const MIN = 0.01;
 const MAX = 0.99;
+const Q = 0.25;
+const D = 0.1;
+const N = 0.05;
+const P = 0.01;
+let numberQ = 0;
+let numberD = 0;
+let numberN = 0;
+let numberP = 0;
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid");
 } else {
-    // write your code for making change here
+  while (amount > Q) {
+    numberQ + 1
+    amount - Q
+  while (amount > D)
+    amount = amount - D
+    numberD++
+  while (amount > N)
+    amount = amount - N
+    numberN++
+  while (amount > P)
+    amount = amount - P
+    numberP++
+  }
 }
+console.log(+numberQ+" quarters, "+numberD+" dimes, "+numberN+" nickles, and "+numberP+" pennies.");
